88d21c2a03ab3cf2a3d5a9e5e984cb9921d98578
Muhammad Iqbal <muhammadiqbalid43@gmail.com>
2025-07-03 14:46:19 +0700

    feat: add tanstack query config

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,16 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      gcTime: 1000 * 60 * 10,
+      refetchOnWindowFocus: true,
+      retry: 2,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
